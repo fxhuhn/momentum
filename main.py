0feb11cafde83d39d03f65d9c134efaa5b8f7566
Markus <github@fxhuhn.de>
2024-10-09 10:33:55 +0200

    initial comit

--- main.py
+++ main.py
@@ -1,111 +1,342 @@
+import os
+import pickle
+
+import numpy as np
 import pandas as pd
 import yfinance as yf
 
-from tools import get_stocks, get_symbols, rsi, sma, trade_report, trades_daily
-
-"""
-Screen stocks based on momentum indicators and generate trading signals.
-"""
+QUANTILE_LOW_MOMENTUM = 0.3
+QUANTILE_HIGH_MOMENTUM = 0.9
 
 
-def filter(df: pd.DataFrame) -> pd.DataFrame:
-    # Apply screening criteria
-    df = df[
-        (df["Close"] > 10)
-        & (df["Volume"] > 1_000_000)
-        & (df["atr"] > 2)
-        & (df["atr"] < 6.5)
-        & (df["aroon"] > 0)
-        & (df["sma_200_pct"] > 0)
-        & (df["rsi"] > 50)
-        & (df["Close"] > df["sma_50"])
-        & (df["sma_50"] > df["sma_200"])
-    ]
+class SP_500_stocks:
+    df = None
 
-    # df_signals = df_signals.sort_values("rsi")
-    return df.sort_values("sma_200_pct")
+    def __init__(self, filename="S&P_500_Historical_04-08-2024.csv") -> None:
+        if os.path.isfile(filename):
+            self.df = pd.read_csv(filename, index_col="date")
+            self.df = self.df[self.df.index >= "2000-01-01"]
+            self.df["tickers"] = self.df["tickers"].apply(
+                lambda x: sorted(x.split(","))
+            )
+
+    def get_symbols(self, year: int, month: int, day: int = 1) -> list[str]:
+        if self.df is None:
+            return None
+
+        snap_shot = f"{year}-{month:02}-{day:02}"
+        df2 = self.df[self.df.index <= snap_shot]
+        return df2.tail(1).tickers.values[0]
+
+    def all_symbols(self) -> list[str]:
+        return sorted(set(sum(self.df.tickers.values, [])))
 
 
-def get_stock_signals(date):
-    signals = []
+sp_500_stocks = SP_500_stocks()
 
-    for symbol, df in dfs.items():
-        df["rsi"] = rsi(df.Close, 20)
-        try:
-            signal = df.loc[signal_date].to_dict()
-            signal["symbol"] = symbol
-            signal["date"] = df.loc[signal_date].name
-            signals.append(signal)
-        except Exception:
-            pass
-    return signals
+
+def roc(close: pd.Series, period: int = 10) -> pd.Series:
+    return (close - close.shift(period)) / close.shift(period) * 100
+
+
+def sp_500_list():
+    table = pd.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")[0]
+    return list(table.Symbol.str.replace(".", "-"))
+    # return ["aapl", "nflx", "tsla", "csco"]
+
+
+def load_stocks(symbols):
+    return yf.download(symbols, start="2000-01-01", group_by="ticker", auto_adjust=True)
+
+
+def df_rearrange(df):
+    # Stack the data to move the ticker symbols into the index
+    df = df.stack(level=0, future_stack=True)
+
+    # Set 'Symbol' and 'Date' as multi-index
+    df.index.names = ["Date", "Ticker"]
+
+    return df.reset_index().set_index(["Ticker", "Date"])
+
+
+def get_trendless(df, date):
+    day = df.loc[(date), ["Close", "SMA_100"]].reset_index()
+    day = day[day["Close"] < day["SMA_100"]].Ticker.values
+    return list(day)
+
+
+def get_lower_quantile(df, date, column, threshold):
+    day = df.loc[(date), column].reset_index()
+    day = day[day[column] < day[column].quantile(threshold)].Ticker.values
+    # day = day[day[column] < 0].Ticker.values
+    return list(day)
+
+
+def get_higher_quantile(df, date, column, threshold):
+    day = df.loc[(date), column].reset_index()
+    day = day[day[column] > day[column].quantile(threshold)].Ticker.values
+    # day = day[day[column] < 0].Ticker.values
+    return list(day)
+
+
+def uptrend(df, date):
+    column = "ROC_10"
+    day = df.loc[(date), column].reset_index()
+    day = day[day[column] > 0].Ticker.values
+    return list(day)
+
+
+def add_day_indicators(df):
+    df["SMA_50"] = df.groupby(level=0)["Close"].transform(
+        lambda x: x.rolling(50).mean()
+    )
+    df["SMA_100"] = df.groupby(level=0)["Close"].transform(
+        lambda x: x.rolling(100).mean()
+    )
+
+    df["PCT"] = df.groupby(level=0)["Close"].transform(
+        lambda x: round(x.pct_change() * 100, 1)
+    )
+    df["ROC_10"] = df.groupby(level=0)["Close"].transform(lambda x: roc(x, 7))
+    df["Changes"] = df.groupby(level=0)["PCT"].transform(lambda x: np.sign(x))
+    return df
+
+
+def add_month_indicators(df):
+    for interval in [3, 6, 9, 12]:
+        df[f"changes_{interval}"] = df.groupby(level=1)["Changes_PCT"].transform(
+            (lambda x: x.rolling(interval).sum().shift(1))
+        )
+        df[f"PCT_{interval}"] = df.groupby(level=1)["PCT"].transform(
+            (lambda x: x.rolling(interval).sum().shift(1))
+        )
+
+    df["ROC_12"] = df.groupby(level=1)["Close"].transform(lambda x: roc(x, 12).shift(1))
+    df["SMA_100"] = df.groupby(level=1)["SMA_100"].transform(lambda x: x.shift(1))
+    return df
+
+
+def resample_df(df):
+    df = df.reset_index()
+    df["Month"] = df["Date"].dt.strftime("%y-%m")
+    df = df.groupby(["Month", "Ticker"]).agg(
+        Date=("Date", "last"),
+        Open=("Open", "first"),
+        Close=("Close", "last"),
+        Changes=("Changes", "sum"),
+        PCT=("PCT", "sum"),
+        SMA_100=("SMA_100", "last"),
+        ROC_10=("ROC_10", "first"),
+    )
+
+    df["Changes_PCT"] = df["Changes"] + (df["PCT"] / 100000)
+
+    return df
+
+
+def convert_to_multiindex(df: pd.DataFrame) -> pd.DataFrame:
+    # Stack the data to move the ticker symbols into the index
+    df = df.stack(level=0, future_stack=True)
+    df.index.names = ["Date", "Ticker"]
+
+    # Set 'Ticker' and 'Date' as multi-index
+    return df.reset_index().set_index(["Ticker", "Date"])
+
+
+def add_indicator_day(df: pd.DataFrame) -> pd.DataFrame:
+    df["SMA"] = df.groupby(level=0)["Close"].transform(lambda x: x.rolling(100).mean())
+    df["ROC_7"] = df.groupby(level=0)["Close"].transform(lambda x: roc(x, 7))
+
+    df["PCT"] = df.groupby(level=0)["Close"].transform(
+        lambda x: round(x.pct_change() * 100, 1)
+    )
+    df["Changes"] = df.groupby(level=0)["PCT"].transform(lambda x: np.sign(x))
+
+    return df
+
+
+def resample_month(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.reset_index()
+
+    df["Month"] = df["Date"].dt.strftime("%y-%m")
+
+    df = df.groupby(["Month", "Ticker"]).agg(
+        Date=("Date", "last"),
+        Open=("Open", "first"),
+        Close=("Close", "last"),
+        Changes=("Changes", "sum"),
+        PCT=("PCT", "sum"),
+        SMA=("SMA", "last"),
+        ROC_7=("ROC_7", "first"),
+    )
+
+    df["Changes_pct"] = df["Changes"] + (df["PCT"] / 100000)
+    return df
+
+
+def add_indicator_month(df: pd.DataFrame) -> pd.DataFrame:
+    df["ROC_12"] = df.groupby(level=1)["Close"].transform(lambda x: roc(x, 12).shift(1))
+    df["SMA"] = df.groupby(level=1)["SMA"].transform(lambda x: x.shift(1))
+
+    for interval in [3, 6, 9, 12]:
+        df[f"Changes_{interval}"] = df.groupby(level=1)["Changes_pct"].transform(
+            (lambda x: x.rolling(interval).sum().shift(1))
+        )
+        df[f"PCT_{interval}"] = df.groupby(level=1)["PCT"].transform(
+            (lambda x: x.rolling(interval).sum().shift(1))
+        )
+
+    return df
+
+
+def pre_processing(df: pd.DataFrame) -> pd.DataFrame:
+    df = convert_to_multiindex(df)
+    df = add_indicator_day(df)
+    df = resample_month(df)
+    df = add_indicator_month(df)
+    return df
+
+
+def sp_500_ticker(year_month: str) -> list:
+    year = 2000 + int(year_month[:2])
+    month = year_month[-2:]
+    return sp_500_stocks.get_symbols(year, month)
+
+
+def match_available_ticker(df_ticker: list, sp_500_ticker: list) -> list:
+    return list(set(sp_500_ticker).intersection(df_ticker))
+
+
+def strategy(df) -> pd.DataFrame:
+    MAX_TICKER = 10
+    QUANTILE_LOW_MOMENTUM = 0.3
+    QUANTILE_HIGH_MOMENTUM = 0.9
+
+    def lower_quantile(df: pd.DataFrame, column, threshold):
+        df = df[column]
+        ticker = df[df < df.quantile(threshold)].index
+        return list(ticker)
+
+    def higher_quantile(df: pd.DataFrame, column, threshold):
+        df = df[column]
+        ticker = df[df > df.quantile(threshold)].index
+        return list(ticker)
+
+    def trendless(df: pd.DataFrame):
+        df = df[["Close", "SMA"]]
+        ticker = df[df.Close < df.SMA].index
+        return list(ticker)
+
+    def downtrend(df):
+        df = df[["ROC_7"]]
+        ticker = df[df.ROC_7 < 0].index
+        return list(ticker)
+
+    ticker = []
+    for changes_idx in [6, 9]:
+        ticker = ticker + lower_quantile(
+            df,
+            f"Changes_{changes_idx}",
+            QUANTILE_LOW_MOMENTUM,
+        )
+
+    for pct_idx in [3]:
+        ticker = ticker + higher_quantile(
+            df,
+            f"PCT_{pct_idx}",
+            QUANTILE_HIGH_MOMENTUM,
+        )
+    ticker = ticker + trendless(df)
+    ticker = ticker + downtrend(df)
+
+    ticker = list(set(df.index.unique()) - set(ticker))
+    return df.loc[ticker]["ROC_12"].nlargest(MAX_TICKER).index
+
+
+def backtest(df: pd.DataFrame):  # -> tuple(pd.DataFrame, float):
+    trade_ticker = {}
+    start = 10_000
+    change_matrix = []
+
+    for year_month in df.reset_index().Month.unique():
+        available_ticker = sp_500_ticker(year_month)
+        monthly_ticker = match_available_ticker(
+            df_ticker=df.reset_index().Ticker.unique(),
+            sp_500_ticker=available_ticker,
+        )
+
+        trade_ticker[year_month] = strategy(
+            df.loc[
+                (year_month, monthly_ticker),
+                :,
+            ]
+            .reset_index()
+            .drop("Month", axis=1)
+            .set_index("Ticker")
+        )
+
+    for year_month, ticker in trade_ticker.items():
+        trades = df.loc[(year_month, ticker), ["Open", "Close"]]
+        if len(trades) > 0:
+            trades["Profit"] = (trades.Close - trades.Open) / trades.Open * 100
+            trades["qty"] = (start / len(trades)) // trades.Open
+            trades["Gewinn"] = (trades.Close - trades.Open) * trades.qty
+            gewinn = trades.Gewinn.sum()
+
+            trades.to_csv(f"./data/trades/{year_month}.csv", header=True, mode="w")
+
+            change_matrix = change_matrix + [
+                list((year_month[:2], year_month[-2:], trades.Profit.mean()))
+            ]
+        else:
+            gewinn = 0
+        start = start + gewinn
+
+    change_matrix = pd.DataFrame(
+        change_matrix, columns=["Year", "Month", "Change"]
+    ).set_index(["Year", "Month"])
+    change_matrix.to_csv("./data/change_matrix.csv", header=True, mode="w")
+
+    return change_matrix, gewinn
+
+
+def load_sp500_stocks(cache: bool = False) -> pd.DataFrame:
+    if cache:
+        with open("./data/stocks.pkl", "rb") as file:
+            df = pickle.load(file)
+    else:
+        df = load_stocks(sp_500_stocks.all_symbols())
+        df.to_pickle("./data/stocks.pkl")
+
+    return df
+
+
+def main() -> None:
+    stocks = load_sp500_stocks(cache=True)
+    stocks = pre_processing(stocks)
+
+    # reduce Data for backtest
+    stocks = stocks.loc[stocks.reset_index().Month.unique()[-190:]]
+
+    trade_matrix, profit = backtest(stocks)
+
+    output = trade_matrix.unstack(level=1)
+    output.loc[:, "Total"] = output.mean(axis=1)
+    output.loc["Total", :] = output.mean(axis=0)
+    output = output.round(2)
+
+    with open("matrix.txt", "w") as text_file:
+        text_file.write(
+            output.to_markdown(floatfmt=".2f")
+            .replace("(", " ")
+            .replace(")", " ")
+            .replace("'", " ")
+            .replace("Change ,", "        ")
+        )
+    print(output)
+
+    print(profit)
 
 
 if __name__ == "__main__":
-    START = "2010-01-01"
-    trades = []
-    last_day = None
-
-    dfs = get_stocks(get_symbols().to_frame("symbol"))
-
-    date_range = dfs[list(dfs.keys())[0]][START:].index
-    spy = yf.download("spy")
-    spy["Long"] = spy.Close > sma(spy.Close, 200)
-
-    for signal_date in date_range:
-        df_signals = pd.DataFrame()
-
-        if spy.loc[signal_date].Long:
-            signals = get_stock_signals(signal_date)
-            df_signals = filter(pd.DataFrame(signals))
-
-            # initial last day data
-            if last_day is None:
-                last_day = df_signals[-10:].copy()
-                last_day["days"] = -1
-
-            if len(last_day) > 0:
-                keep = pd.merge(
-                    df_signals[-15:], last_day[["symbol", "days"]], on="symbol"
-                )
-                keep["days"] += 1
-            else:
-                # no trades yesterday
-                keep = pd.DataFrame()
-
-            if len(keep) < 10:
-                # add some new candidates
-                df_signals = pd.concat(
-                    [
-                        keep,
-                        pd.concat([keep, df_signals]).drop_duplicates(
-                            subset="symbol", keep=False
-                        )[len(keep) - 10 :],
-                    ],
-                    ignore_index=True,
-                )
-            else:
-                df_signals = keep
-
-            if len(df_signals) > 0:
-                try:
-                    df_signals["days"] = df_signals.days.fillna(0)
-                except Exception:
-                    df_signals["days"] = 0
-                df_signals.loc[df_signals["days"] == 0.0, "start"] = df_signals["Date"]
-            else:
-                df_signals = pd.DataFrame()
-
-        last_day = df_signals
-        trades.append(df_signals)
-
-    trades = pd.concat(trades, ignore_index=True)
-
-    for symbol in trades.symbol.to_list():
-        trades.loc[trades.symbol == symbol, "start"] = trades[
-            trades.symbol == symbol
-        ].start.ffill()
-
-    # export Reports
-    trades_daily(trades)
-    trade_report(trades)
+    main()
